99de2b39a0a02062e63d5362d647f790ec6e1350
AlirezaAkbarpour <akbarpouralireza1>
2024-01-27 01:04:56 +0330

    vars

--- main.rs
+++ main.rs
@@ -1,3 +1,12 @@
 fn main(){
-    println!("hello world!")
+    println!("hello world!");
+
+    //variables 
+    let x = 5;
+    println!("x: {x}");
+    //mutable vars or change ability 
+    let mut y = 0;
+    println!("y: {y}");
+    y=3;
+    println!("y after change: {y}");
 }
