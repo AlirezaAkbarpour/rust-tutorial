592cc8620a8edb922ff56200e2d0481f347d82f3
AlirezaAkbarpour <akbarpouralireza1>
2024-02-29 12:08:50 +0330

    add random number

--- main.rs
+++ main.rs
@@ -1,12 +1,15 @@
 use std::io;
+use rand::{thread_rng, Rng};
 
 fn main(){
     println!("Hello World!");
     println!("Please input your guess!");
 
+    let secret_num = thread_rng().gen_range(1..=100);
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed : {guess}");
+    println!("secret Number : {secret_num}");
 }
